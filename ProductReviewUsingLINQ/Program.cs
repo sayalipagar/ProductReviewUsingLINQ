a326c6c1a2441797bac2cff88a3a70106fffed44
sayalipagar <pagarsayali565@gmail.com>
2022-11-04 15:11:04 +0530

    [Sayali]Add:Welcome to LINQ ProblemStatement

---
+++ ProductReviewUsingLINQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReviewUsingLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!!!!!");
+        }
+    }
+}
